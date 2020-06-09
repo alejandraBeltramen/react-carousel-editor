8b9f999a4bdae74cd35f18aa05651046bbf850a8
Alejandra Beltramen <alejandra.beltramen@gmail.com>
2020-06-09 18:42:23 -0300

    Creation of image selector, update App.js just for testing, new color variables and Card component updated

---
+++ src/components/ImageSelector/ImageSelector.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ImageSelector.scss';
+import Card from '../../ui-core/Card/Card';
+import Button from '../../ui-core/Button/Button';
+
+const ADD = 'Add';
+const imageSelector = (props) => {
+  const imagesToRender = props.images.map((image, index) => 
+    <div className="is_container__card"
+         key={index}
+         onClick={props.onCardClick}>
+      <Card source={image.imageName}
+            caption={image.imageCaption}
+            isSelected={image.isSelected}>
+      </Card>
+    </div>
+  );
+
+  return (
+    <div className="image-selector">
+      <div className="is__container">
+        { imagesToRender }
+      </div>
+
+    <Button onClick={props.onAddClick}>{ ADD }</Button>
+    </div>
+  );
+};
+
+export default imageSelector;
